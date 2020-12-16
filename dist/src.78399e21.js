6b59faa0e8298c8bcee800eaa619ed7ac07b675a
Matt Dillemuth <mattdillemuth@gmail.com>
2020-12-15 21:23:57 -0500

    Update repo to prepare for combining with backend

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -41542,7 +41542,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44867" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40757" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
