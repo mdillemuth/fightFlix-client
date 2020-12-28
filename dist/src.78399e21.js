d02a180efd2732152dca4a29f251dd7a93ebc973
Matt Dillemuth <mattdillemuth@gmail.com>
2020-12-27 23:32:31 -0500

    Add parcel-bundler to dependencies for building on Netlify

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -72273,7 +72273,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34179" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41683" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
